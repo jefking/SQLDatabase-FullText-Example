6ae6571234161095bf8b086e64e799fef83bbed6
Jef King <jefkin@microsoft.com>
2015-10-18 21:20:57 -0700

    cHECK IT

---
+++ Fulltext/FullTextIndex1.sql
@@ -0,0 +1,4 @@
+﻿CREATE FULLTEXT INDEX
+	ON [dbo].[Table1] (texty)
+	KEY INDEX [PK_Table1]
+	WITH CHANGE_TRACKING AUTO
---
+++ Fulltext/Table1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL , 
+    [texty] NTEXT NULL, 
+    CONSTRAINT [PK_Table1] PRIMARY KEY ([Id])
+)
+
+GO
